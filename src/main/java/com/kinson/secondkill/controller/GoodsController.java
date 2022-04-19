d5efec8a95087fe0961f6a51752d369b4f6b813c
fujiangwei <2219945910@qq.com>
2022-04-19 14:01:00 +0800

    商品列表&详情&cookie

--- src/main/java/com/kinson/secondkill/controller/GoodsController.java
+++ src/main/java/com/kinson/secondkill/controller/GoodsController.java
@@ -1,16 +1,19 @@
 package com.kinson.secondkill.controller;
 
+import com.kinson.secondkill.domain.UserEntity;
+import com.kinson.secondkill.domain.vo.GoodsVo;
 import com.kinson.secondkill.service.IGoodsService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  * @Description: 商品控制器
@@ -26,17 +29,48 @@ public class GoodsController {
     private IGoodsService goodsService;
 
     @RequestMapping("/toList")
-    public String toLogin(HttpSession session, Model model/*, @CookieValue("userTicket") String userTicket*/) {
-        /*if (StringUtils.isEmpty(userTicket)) {
+    public String toLogin(HttpSession session, Model model, @CookieValue("userTicket") String userTicket) {
+        if (StringUtils.isEmpty(userTicket)) {
             return "login";
         }
 
-        User user = (User) session.getAttribute(userTicket);
+        UserEntity user = (UserEntity) session.getAttribute(userTicket);
         if (null == user) {
             return "login";
-        }*/
-        // model.addAttribute("user", user);
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("goodsList", goodsService.findGoodsVo());
         return "goodsList";
     }
+
+    @RequestMapping("/toDetail/{goodsId}")
+    public String toDetail(Model model, UserEntity user, @PathVariable Long goodsId) {
+        model.addAttribute("user", user);
+        GoodsVo goods = goodsService.findGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+        Date startDate = goods.getStartDate();
+        Date endDate = goods.getEndDate();
+        Date nowDate = new Date();
+        // 秒杀状态
+        int secKillStatus = 0;
+        // 剩余开始时间
+        int remainSeconds = 0;
+        if (nowDate.before(startDate)) {
+            // 秒杀还未开始
+            remainSeconds = (int) ((startDate.getTime() - nowDate.getTime()) / 1000);
+        } else if (nowDate.after(endDate)) {
+            // 秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+        } else {
+            // 秒杀中
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("secKillStatus", secKillStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        return "goodsDetail";
+    }
+
 
 }
